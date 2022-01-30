b8801d36afe4d85fe43f9f3c78ff54af57a2a062
Lance Barisdale <lance.barisdale@gmail.com>
2022-01-29 20:50:52 -0800

    first commit

---
+++ test/first.spec.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+describe('A feature', function(){
+    describe('A scenario', function(){
+      describe('does something expected', function(){
+        assert(true);
+      });
+      
+    });
+    
+});
